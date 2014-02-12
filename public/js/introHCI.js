95691fe1de1ad296844beced111aeac3161752ae
Mark Valentine <markolas@stanford.edu>
2014-02-12 15:32:00 -0800

    ajax lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -27,6 +27,19 @@ function addProjectDetails(e) {
 	var idNumber = projectID.substr('project'.length);
 
 	console.log("User clicked on project " + idNumber);
+
+	var url = "/project/" +idNumber;
+	console.log(url);
+	$.get(url, addProjectStuff);
+}
+
+
+function addProjectStuff(result){
+	console.log(result);
+	var inserthtml ='<img src="' + result['image'] +
+		'" class="detailsImage">' + '<p><small>' + result['date'] +
+		'</small></p>' + '<p>' + result['summary'] + '</p>';
+	$("#project"+result['id']+ " .details").html(inserthtml);
 }
 
 /*
@@ -35,4 +48,14 @@ function addProjectDetails(e) {
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+	$.get("/palette", randomPalette);
+}
+
+function randomPalette(result) {
+	var colors = result['colors']['hex'];
+	$("body").css('background-color', colors[0]);
+	$('.thumbnail').css('background-color', colors[1]);
+	$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+	$('p').css('color', colors[3]);
+	$('.project img').css('opacity', .75);
 }
